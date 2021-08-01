2495d787926fc69ffd54efdd48ac0d6de9b0de1a
Tibix <polgartibi76@gmail.com>
2021-08-01 14:06:45 +0200

    Finished version

--- task.sql
+++ task.sql
@@ -1,4 +1,4 @@
--- Elso megoldasi lehetoseg
+-- First solution
 
 WITH cte_category 
 AS
@@ -12,7 +12,7 @@ where pc.Name='Bikes'
 select * from cte_category
 order by Size, ListPrice desc
 
--- Masodik megoldasi lehetoseg
+-- Second solution
 
 WITH cte_category (
 category_id)
@@ -24,5 +24,3 @@ and pc.Name='Bikes')
 select top 1 pp.name,pp.Size,pp.ListPrice from Production.Product pp , cte_category
 where pp.ProductSubcategoryID=cte_category.category_id
 order by pp.Size, pp.ListPrice desc
-
---Nem feltétlen a legjobbak, de az eredményt megadja. Az lehetne a megoldás, hogy top 1 nélkül hogy oldható meg.
